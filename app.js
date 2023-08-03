a4bba33714afd5bcfe6ac70b30fa0b713f390c77
TechnoGeek01 <rsudharsangames@gmail.com>
2023-08-03 14:53:18 +0530

    product review functionality added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
 const productRouter = require("./routes/productRoutes");
+const reviewRouter = require("./routes/reviewRoutes");
 require("dotenv").config();
 require("express-async-errors");
 
@@ -34,6 +35,7 @@ app.get("/api/v1", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/reviews", reviewRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
